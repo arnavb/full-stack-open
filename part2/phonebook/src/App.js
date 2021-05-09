59642914821ae83b4be0d077464c476e60d1aa1c
Arnav Borborah <arnavborborah11@gmail.com>
2021-05-09 11:47:59 -0400

    2.9: Add functionality to filter names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "12345-6789" },
+    { name: "Arto Hellas", phoneNumber: "040-123456" },
+    { name: "Ada Lovelace", phoneNumber: "39-44-5323523" },
+    { name: "Dan Abramov", phoneNumber: "12-43-234345" },
+    { name: "Mary Poppendieck", phoneNumber: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -23,6 +27,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter by name:{" "}
+      <input
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
+      />
+      <h2>Add a new person</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:{" "}
@@ -42,11 +52,15 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <li key={person.name}>
-          {person.name} {person.phoneNumber}
-        </li>
-      ))}
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map((person) => (
+          <li key={person.name}>
+            {person.name} {person.phoneNumber}
+          </li>
+        ))}
     </div>
   );
 };
